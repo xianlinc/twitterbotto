f88e75d4c0ca39384107b12492cc6e756cf9b1bc
xianlin <xianlinchng@gmail.com>
2021-10-30 00:41:38 +0800

    Change telegram bot response to non command

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ def main():
     dp.add_handler(CommandHandler("stalk", stalk))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler(Filters.text, "Please input a valid command!"))
 
     # log all errors
     dp.add_error_handler(error)
