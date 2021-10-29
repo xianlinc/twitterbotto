8c02b2f433ec4b4f1ee924fd8a1ddb0a4d34f64c
xianlin <xianlinchng@gmail.com>
2021-10-30 00:53:50 +0800

    Fix bug where string was passed into where a method was needed

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ def help(update, context):
 
 def echo(update, context):
     """Echo the user message."""
-    update.message.reply_text(update.message.text)
+    update.message.reply_text("Please input a valid command!")
 
 
 def error(update, context):
@@ -81,7 +81,7 @@ def main():
     dp.add_handler(CommandHandler("stalk", stalk))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, "Please input a valid command!"))
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(error)
