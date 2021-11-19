a056205e7728b2e92d9f43d221fc126b1b784658
xianlin <xianlinchng@gmail.com>
2021-11-19 19:16:57 +0800

    Remove bot echo and add test command

--- bot.py
+++ bot.py
@@ -34,20 +34,14 @@ logger = logging.getLogger(__name__)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+def test(update, context):
+    """Send a message when the command /test is issued."""
+    update.message.send_message('Bot is up')
 
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
-
-
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text("Please input a valid command!")
-
+    update.message.send_message('Help!')
 
 def error(update, context):
     """Log Errors caused by Updates."""
